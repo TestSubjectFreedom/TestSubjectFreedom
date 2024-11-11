28271d47f5fb43c1d4457d283d3cc4fef3abdad1
Artur <bodzio.entire@gmail.com>
2024-11-11 18:29:35 +0100

    Base magnet object with Gameplay Tags

--- Source/TestSubjectFreedom/TestSubjectFreedom.Build.cs
+++ Source/TestSubjectFreedom/TestSubjectFreedom.Build.cs
@@ -4,10 +4,10 @@ using UnrealBuildTool;
 
 public class TestSubjectFreedom : ModuleRules
 {
-	public TestSubjectFreedom(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+    public TestSubjectFreedom(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
-	}
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayTags" });
+    }
 }
